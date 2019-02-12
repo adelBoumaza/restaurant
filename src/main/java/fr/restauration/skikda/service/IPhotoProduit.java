51ad226390311e9cde0bfcfffa921644252ecd2b
aboumaza <ad.boumaza@yahoo.fr>
2019-02-12 22:58:08 +0100

    new commit

--- src/main/java/fr/restauration/skikda/service/IPhotoProduit.java
+++
@@ -1,5 +0,0 @@
-package fr.restauration.skikda.service;
-
-public interface IPhotoProduit {
-
-}
--- src/test/java/fr/restauration/skikda/ProjetRestaurationApplicationTests.java
+++ src/test/java/fr/restauration/skikda/ProjetRestaurationApplicationTests.java
@@ -12,9 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import fr.restauration.skikda.entities.Categorie;
+import fr.restauration.skikda.entities.PhotoProduit;
 import fr.restauration.skikda.entities.Role;
 import fr.restauration.skikda.entities.User;
 import fr.restauration.skikda.service.ICategorieService;
+import fr.restauration.skikda.service.IPhotoProduitService;
 import fr.restauration.skikda.service.IUserService;
 
 @RunWith(SpringRunner.class)
@@ -27,11 +29,22 @@ public class ProjetRestaurationApplicationTests {
 	@Autowired
 	private ICategorieService iCategorieService;
 	
+	@Autowired
+	private IPhotoProduitService iPhotoProduit;
+	
 	@Test
+	@Ignore
 	public void contextLoads() {
 		Categorie categorie = new Categorie();
 		categorie.setNomCategorie("Salade");
 		assertNotNull(iCategorieService.ajouterCategorie(categorie));
+	}
+	
+	@Test
+	public void ajouterPhotoProduit() {
+		PhotoProduit photoProduit = new PhotoProduit();
+		photoProduit.setPhotoProduit(new byte[] {});
+		assertNotNull(iPhotoProduit.ajouterPhotoProduit(photoProduit).getId());
 	}
 
 }
